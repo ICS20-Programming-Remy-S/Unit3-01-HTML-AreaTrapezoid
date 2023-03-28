fead8bb80d38059158b5970cf6811059d80cd635
Remy Skelton <124379820+Messi-Sui@users.noreply.github.com>
2023-03-28 14:42:13 +0000

    Initial commit

--- js/script.js
+++ js/script.js
@@ -5,16 +5,14 @@
 // This file contains the JS functions for index.html
 
 function enterClicked () {
-  const TAX_RATE=0.2005
   
   // User data input
-  let hoursWorked = parseFloat(document.getElementById("hours-worked").value)
-  let hourlyRate = parseFloat(document.getElementById("hourly-rate").value)
-  let totalPay = hoursWorked * hourlyRate;
-  let tax = totalPay * TAX_RATE;
-  let takeHomeSalary = totalPay - tax;
+  let b1Side = parseFloat(document.getElementById("b1-side").value)
+  let b2Side = parseFloat(document.getElementById("b2-side").value)
+  let height = parseFloat(document.getElementById("height").value)
+  //prosses
+  let area = ((b1Side + b2Side) * height) / 2;
   
   // Data given back to user
-  document.getElementById('takeHomeSalary').innerHTML = 'After taxes your take home slary is $' + takeHomeSalary.toFixed(2)
-  document.getElementById('taxPaid').innerHTML = 'Amount of tax money the goverment will resive from you is $ ' + tax.toFixed(2)
+  document.getElementById('area').innerHTML = 'The area of your trapezoid is ' + area.toFixed(2) + 'cm<sup>2</sup>'
 }
